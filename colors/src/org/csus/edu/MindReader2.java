f4576db54073c97b6e5872bea9a7ca09b7063992
CJ Angelina <cj_angelina@yahoo.com>
2019-02-27 18:33:35 -0800

    My first commit to Github

---
+++ colors/src/org/csus/edu/MindReader2.java
@@ -0,0 +1,45 @@
+package org.csus.edu;
+
+import java.util.Scanner;
+
+public class MindReader2 {
+
+	public static void main(String[] args) {
+		System.out.println("Choose a number in your mind. I will state that number. \nEnter yes after you are done");
+		
+		process();
+		
+		System.out.println("*****************************************************");
+		System.out.println("\n\nThe result is -------> 2   :)");
+		
+	}
+	public static void process() {
+	
+		Scanner sc = new Scanner(System.in);
+		sc.next();
+		String ch = "no";
+		do {
+			System.out.println("\nAdd 5 to it.\n Are you done ?");
+			sc.next();
+		} while (ch.equals("yes"));
+		do {
+			System.out.println("\nMultiply the result by 3.\n Are you done ?");
+			sc.next();
+		} while (ch.equals("yes"));
+		do {
+			System.out.println("Subtract 9 from the result.\n Are you done ?");
+			sc.next();
+		} while (ch.equals("yes"));
+		do {
+			System.out.println("Divide the result by 3.\n Are your done ?");
+			sc.next();
+		} while (ch.equals("yes"));
+		do {
+			System.out.println("Subtract the number that was first selected.\n Are you done ?");
+			sc.next();
+		} while (ch.equals("yes"));
+		
+	}
+
+}
+
